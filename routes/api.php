b167a32fe300fb25abf7dddd49edcbb94c1fde7a
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2025-01-24 21:11:58 +0700

    add clients endpoint to API with authentication

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Models\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/clients', function () {
+    $clients = Client::all();
+    return response()->json(['data' => $clients]);
+});
 
 Route::post('/login', [AuthController::class, 'login']);
